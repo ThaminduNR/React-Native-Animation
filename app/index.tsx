b569ca9bc7227707b7caf2f72f4dced3738feeb3
thaminduranawaka <thaminduniwantha777@gmail.com>
2025-03-18 16:58:36 +0530

    create circle animation

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import CircularSlider from "@/app-example/components/CircularSlider";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <CircularSlider />
+    </View>
+  );
+}
